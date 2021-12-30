d78612fb0acd56aaa3b38afed857e207cc1d71d9
spiedra <jcspiedra@gmail.com>
2021-12-30 12:26:24 -0600

    feat: add breakpoint sizes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -11,6 +11,15 @@ const theme = {
     medium: "2em",
     large: "3em",
   },
+  bp: {
+    mobileS: `(min-width: 320px)`,
+    mobileM: `(min-width: 375px)`,
+    mobileL: `(min-width: 425px)`,
+    tablet: `(min-width: 768px)`,
+    laptop: `(min-width: 1024px)`,
+    laptopL: `(min-width: 1440px)`,
+    laptopXL: `(min-width: 2560px)`,
+  },
 };
 
 export default theme;
