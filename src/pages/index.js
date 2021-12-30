e5bf2705aeacb6e53541aa5c79ad0d2d7d8442aa
spiedra <jcspiedra@gmail.com>
2021-12-30 12:07:21 -0600

    feat: add theme file with default variables

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,25 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  border: ${(props) => `1px solid ${props.theme.colors.onyx}`};
+  background-color: ${(props) => props.theme.colors.lightBlue};
+  font-family: ${(props) => props.theme.fonts[0]};
+`;
+
+const Heading = styled.h1`
+  font-size: ${({ isHeading, theme: { fontSizes } }) =>
+    isHeading ? fontSizes.large : fontSizes.small};
+  color: ${({ theme: { colors } }) => colors.persianGreen};
+`;
+
 export default function Home() {
   return (
     <>
-      <h1>Clean up!</h1>
+      <Container>
+        <Heading isHeading={true}>Hello World</Heading>
+        <h2>By the power of styled-components!</h2>
+      </Container>
     </>
   );
 }
