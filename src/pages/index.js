a2b0ee4567a385efe4b0a88312b1b7b45bb9cf8d
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2021-12-30 13:46:01 -0600

    Feat/app page theme (#8)
    PR Close #8

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,19 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  ${props => props.theme.mixins.flexCenter}
+  
+  @media ${(props) => props.theme.bp.laptop}{
+    background-color: red;
+  }
+`;
+
 export default function Home() {
   return (
     <>
-      <h1>Clean up!</h1>
+      <Container>
+        <h2>By the power of styled-components!</h2>
+      </Container>
     </>
   );
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+import mixins from './mixins';
+
+const theme = {
+  bp: {
+    mobileS: `(min-width: 320px)`,
+    mobileM: `(min-width: 375px)`,
+    mobileL: `(min-width: 425px)`,
+    tablet: `(min-width: 768px)`,
+    laptop: `(min-width: 1024px)`,
+    laptopL: `(min-width: 1440px)`,
+    laptopXL: `(min-width: 2560px)`,
+  },
+  mixins,
+};
+
+export default theme;
