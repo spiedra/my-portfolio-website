2d1353ccc2a6f42ad4734f3bbffddc34198b58c0
spiedra <jcspiedra@gmail.com>
2021-12-31 08:37:13 -0600

    feat: add normalize.css last version

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,12 +1,13 @@
-import styled, { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
 import variables from "./variables";
 
 const GlobalStyle = createGlobalStyle`
+  ${normalize}
   ${variables}
 
   body {
-    padding: 0;
-    margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
