8597cc8299907741b8ffee73f1ea1b317349acb9
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2021-12-31 09:13:12 -0600

    Feat/add page layout (#11)
    PR close #11

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,23 +1,33 @@
-import styled, { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
 import variables from "./variables";
 
 const GlobalStyle = createGlobalStyle`
+  ${normalize}
   ${variables}
 
   body {
-    padding: 0;
-    margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 
-  a {
-    color: inherit;
-    text-decoration: none;
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    min-height: 100vh;
+    flex: 1;
   }
 
-  * {
-    box-sizing: border-box;
+  footer {
+    display: flex;
+    flex: 1;
+    padding: 0.3rem 0;
+    border-top: 1px solid var(--green-cyan);
+    justify-content: center;
+    align-items: center;
   }
 `;
 
