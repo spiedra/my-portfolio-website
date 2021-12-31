6cfe28a679886b7faa9327a45e378b35128908e4
spiedra <jcspiedra@gmail.com>
2021-12-31 09:19:45 -0600

    refactor: add main component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
+import Main from "./Main";
 
 export default function Layout({ children }) {
   return (
@@ -14,7 +15,7 @@ export default function Layout({ children }) {
         />
       </Head>
       <Header />
-      <main>{children}</main>
+      <Main>{children}</Main>
       <Footer />
     </>
   );
