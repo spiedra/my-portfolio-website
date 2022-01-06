73a70f80b6bfea81d5c6fdbfe8a59329172d0370
spiedra <jcspiedra@gmail.com>
2022-01-06 15:40:46 -0600

    feat: add responsiveness to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,41 @@ import styled from "styled-components";
 import SvgDeveloper from "../components/SvgComponents/SvgDeveloper";
 
 const LandingSection = styled.section`
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  width: 85%;
-  min-height: 90vh;
-  padding: 10px 0;
+  ${(props) => props.theme.mixins.flexCenter}
+  flex-direction: column-reverse;
+  width: 90%;
+  padding-top: 5rem;
+
+  @media ${(props) => props.theme.bp.tablet} {
+    flex-direction: row;
+    min-height: 10vh;
+    width: 85%;
+  }
 `;
 
-const IntroContainer = styled.article``;
+const IntroContainer = styled.article`
+  max-width: 80%;
+  font-size: 18px;
+  text-align: center;
+`;
 
 const MainImageContainer = styled.div`
-    width: 50%;
-    height: 50%;
-    ${(props) => props.theme.mixins.flexCenter}
+  ${(props) => props.theme.mixins.flexCenter}
+  width: 15.625rem;
+  height: 14.0625rem;
 `;
 
-const HiMessage = styled.p``;
+const HiMessage = styled.p`
+  margin-top: .7rem;
+`;
 
-const MyName = styled.h1``;
+const MyName = styled.h1`
+  color: var(--chetwode-blue);
+`;
 
 const Presentation = styled.p``;
+
+const CurriculumButton = styled.button``;
 
 export default function Home() {
   return (
@@ -32,9 +46,10 @@ export default function Home() {
           <HiMessage>Hi, my name is</HiMessage>
           <MyName>Juan Carlos Sequeira Piedra</MyName>
           <Presentation>I am a business informatics student</Presentation>
+          <CurriculumButton />
         </IntroContainer>
         <MainImageContainer>
-          <SvgDeveloper/>
+          <SvgDeveloper />
         </MainImageContainer>
       </LandingSection>
     </>
