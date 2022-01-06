3bf9a3feda5bbb7c8d2c1e2c0aecb2157ab2ada3
spiedra <jcspiedra@gmail.com>
2022-01-06 11:38:13 -0600

    feat: add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,6 +32,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;500;600&display=swap"
             rel="stylesheet"
