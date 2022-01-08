a3afbfa48e540f794fcf01347818006d93b95aba
spiedra <jcspiedra@gmail.com>
2022-01-08 13:59:08 -0600

    content: update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,23 @@
+import styled from "styled-components";
+import SocialMediaBox from "./SocialMediaBox";
+
+const FooterContainer = styled.footer`
+  ${(props) => props.theme.mixins.flexCenter}
+  flex-direction: column;
+  font-size: 24px;
+  text-align: center;
+`;
+
+const FinalMessage = styled.span`
+  font-size: 13px;
+  padding-bottom: 1.4rem;
+`;
+
 export default function Footer() {
   return (
-    <footer>
-      <p>This is the footer</p>
-    </footer>
+    <FooterContainer>
+      <SocialMediaBox />
+      <FinalMessage>© Jcspiedra - San José, Costa Rica 2021</FinalMessage>
+    </FooterContainer>
   );
 }
