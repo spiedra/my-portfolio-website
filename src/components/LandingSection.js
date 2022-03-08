56517620ee0e942ec70a9439ec2a520eec3449d4
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-03-08 01:15:19 -0600

    content: update cvButton link

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -92,7 +92,7 @@ export default function LandingSection() {
         <MyName>Juan Carlos Sequeira Piedra</MyName>
         <p>I am a business computing student</p>
         <CurriculumButton
-          href="https://drive.google.com/file/d/1IUaPrteSRkCpfd9Us-EeKkdPBOVGeXLT/view?usp=sharing"
+          href="https://drive.google.com/file/d/14dpRlpYlf_cmyzxN9yGN8SWYjg0u3nV2/view?usp=sharing"
           target="_blank"
           rel="noreferrer"
         >
